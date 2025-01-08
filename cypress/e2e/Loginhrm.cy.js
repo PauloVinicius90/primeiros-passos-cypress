a7e2f40be0acd2ffa4a782ee2d3e98dd9b8ed8e9
Paulo Vinicius <pauloviniciusgdasilva@gmail.com>
2025-01-08 12:03:37 -0300

    Adding user data json

--- cypress/e2e/Loginhrm.cy.js
+++ cypress/e2e/Loginhrm.cy.js
@@ -1,17 +1,36 @@
+
 describe('Orange HRM login', () => {
 
+  const selectorsList = {
+    usernameField: "[name='username']",   // criando uma varialvel  para caso seja alterado os nomes, para facilitar e não alterar todos um por um,para não repetir os seletores, exemplo : usarname muda para nickname
+    passwordField:"[name='password']",
+    loginButton:"[type='submit']",
+  
+  }
+
+  const userdata = {
+    userSuccess: {
+      username: "Admin",
+      password: "admin123"
+    },
+    userFail:{ 
+      username: "test",
+      password: "test"
+    }
+  
+  }
   it('Login success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // site que estamos testando
-    cy.get("[name='username']").type('Admin') // colocar nome do usuario de ADMIN (foi utilizado CSS selector para colocar name =username)
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123') // colocar senha admin123
-    cy.get('.oxd-button').click() // apertar boton de login
+    cy.get(selectorsList.usernameField).type(userdata.userSuccess.username) // colocar nome do usuario de ADMIN (foi utilizado CSS selector para colocar name =username)
+    cy.get(selectorsList.passwordField).type(userdata.userSuccess.password) // colocar senha admin123
+    cy.get(selectorsList.loginButton).click() // apertar boton de login
     cy.location('pathname').should('equal','/web/index.php/dashboard/index') // verificar se foi para a pagina correta após o login
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard') // encontrar a palavra dashboard para confirma se apagina esta correta
+    cy.get('.orangehrm-upgrade-layout') // encontrar a palavra dashboard para confirma se apagina esta correta
   })
   it('Login Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test') // colocado usuario incorreto
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test') // colocado senha incorreta
+    cy.get(selectorsList.usernameField).type(userdata.userFail.username) // colocado usuario incorreto
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(userdata.userFail.password) // colocado senha incorreta
     cy.get('.oxd-button').click() // apertat botão login 
     cy.get('.oxd-alert-content > .oxd-text') // verificar msg de erro
   })
